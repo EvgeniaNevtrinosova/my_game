668bc546b1b04d46bcf712c944ea18b81a719d42
Евгения <jenushkanor@gmail.com>
2018-01-30 17:31:53 +0300

    SPA

--- public/main.js
+++ public/main.js
@@ -1,17 +1,33 @@
-'use strict';
+;(function() {
+    'use strict';
 
-const sections = [
-    ['game', 'Режим игры'],
-    ['rules', 'Правила игры'],
-    ['rating', 'Рейтинг игроков']
-];
+    const sections = [
+        ['game', 'Режим игры'],
+        ['rules', 'Правила игры'],
+        ['rating', 'Рейтинг игроков']
+    ];
 
-const nav = document.getElementById('navigation');
+    const nav = document.getElementsByClassName('navigation')[0];
 
-sections.forEach((section) => {
-    const button = document.createElement('input');
-    button.setAttribute('type', 'button');
-    button.setAttribute('section-name', section[0]);
-    button.value = section[1];
-    nav.appendChild(button);
-});
+    sections.forEach((section) => {
+        const button = document.createElement('input');
+        button.setAttribute('type', 'button');
+        button.setAttribute('section-name', section[0]);
+        button.value = section[1];
+        nav.appendChild(button);
+    });
+
+    const app = document.querySelector('#application');
+    const arrayOfSections = Array.from(app.getElementsByTagName('section'));
+
+    nav.addEventListener('click', (event) => { 
+        if (event.target.nodeName.toLowerCase() !== "input") { 
+            return;
+        }
+
+        const clickSection = event.target.getAttribute('section-name');
+        arrayOfSections.forEach((section) => {
+            section.hidden = (section.id !== clickSection);
+        });
+    });
+})();
